a2e676a0206562288afa973e250e9ab4d026d409
kupaqu <fccfcbyjhtk@gmail.com>
2022-03-05 13:54:06 +0800

    mask

--- dealwithit.py
+++ dealwithit.py
@@ -1,4 +1,6 @@
 import cv2
+from cv2 import IMREAD_UNCHANGED
+import matplotlib.pyplot as plt
 import numpy as np
 
 def detectAndDisplay(frame, face_classifier, eye_classifier, glasses, scaleFactor=None, minNeighbors=None):
@@ -10,7 +12,7 @@ def detectAndDisplay(frame, face_classifier, eye_classifier, glasses, scaleFacto
         result, scaleFactor=scaleFactor, minNeighbors=minNeighbors)
     for (x, y, w, h) in faces_rects:
         face = result[y:y+h, x:x+w]
-        cv2.rectangle(result, (x, y), (x+w, y+h), (255, 255, 255))
+        # cv2.rectangle(result, (x, y), (x+w, y+h), (255, 255, 255))
 
         # ищем глаза внутри лиц
         eyes_rects = eye_classifier.detectMultiScale(
@@ -21,10 +23,10 @@ def detectAndDisplay(frame, face_classifier, eye_classifier, glasses, scaleFacto
         if len(eyes_rects) == 2:
             eyes_rects = sorted(eyes_rects, key=lambda x: x[0]) # сортировка по горизонтали
             # print(eyes_rects)
-            upper_left = np.array([eyes_rects[0][0], eyes_rects[0][1]], dtype=np.float32)
-            bottom_left = np.array([eyes_rects[0][0], eyes_rects[0][1] + eyes_rects[0][3]], dtype=np.float32)
-            bottom_right = np.array([eyes_rects[1][0] + eyes_rects[1][2], eyes_rects[1][1] + eyes_rects[1][3]], dtype=np.float32)
-            upper_right = np.array([eyes_rects[1][0] + eyes_rects[1][2], eyes_rects[1][1]], dtype=np.float32)
+            upper_left = np.array([eyes_rects[0][0]-100, eyes_rects[0][1]-50], dtype=np.float32)
+            bottom_left = np.array([eyes_rects[0][0]-100, eyes_rects[0][1] + eyes_rects[0][3]+50], dtype=np.float32)
+            bottom_right = np.array([eyes_rects[1][0]+100 + eyes_rects[1][2], eyes_rects[1][1] + eyes_rects[1][3]+50], dtype=np.float32)
+            upper_right = np.array([eyes_rects[1][0]+100 + eyes_rects[1][2], eyes_rects[1][1]-50], dtype=np.float32)
             output_pts = np.float32([upper_left, bottom_left, bottom_right, upper_right])
 
             input_pts = np.float32([
@@ -35,20 +37,28 @@ def detectAndDisplay(frame, face_classifier, eye_classifier, glasses, scaleFacto
             ])
 
             M = cv2.getPerspectiveTransform(input_pts, output_pts)
+            
+            mask = glasses[:,:,3]
+            glasses = glasses[:,:,:3]
 
+            mask = cv2.warpPerspective(mask, M, (face.shape[1], face.shape[0]), cv2.INTER_LINEAR)
             glasses = cv2.warpPerspective(glasses, M, (face.shape[1], face.shape[0]), cv2.INTER_LINEAR)
+
             gw, gh, gc = glasses.shape
-            # print(glasses.shape)
             for i in range(0, gw):
                 for j in range(0, gh):
-                    face[i, j] = glasses[i, j]
+                    if mask[i, j] != 0:
+                        face[i, j] = glasses[i, j]
 
-        for (x2, y2, w2, h2) in eyes_rects:
-            cv2.rectangle(face, (x2, y2), (x2+w2, y2+h2), (255, 255, 255))
+        # for (x2, y2, w2, h2) in eyes_rects:
+        #     cv2.rectangle(face, (x2, y2), (x2+w2, y2+h2), (255, 255, 255))
 
     return result
 
-glasses = cv2.imread("dealwithit.png")
+glasses = cv2.imread("dealwithit.png", flags=IMREAD_UNCHANGED)
+print(glasses.shape)
+plt.imshow(glasses)
+plt.show()
 
 face_cascade = "haarcascade_frontalface_default.xml"
 eye_cascade = "haarcascade_eye.xml"
